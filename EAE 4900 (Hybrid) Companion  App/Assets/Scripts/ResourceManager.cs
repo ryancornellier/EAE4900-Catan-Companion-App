500327b63b667242570c1b7af709ca4584907618
Ryan Cornellier <RCTennis15@gmail.com>
2020-02-12 14:41:45 -0700

    :)
    Counters work!

---
+++ EAE 4900 (Hybrid) Companion  App/Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceManager : MonoBehaviour
+{
+    public Button WheatButtonPlus;
+    public Button WheatButtonMinus;
+    public int WheatCounter;
+    public Text WheatText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        WheatCounter = 0;
+        WheatButtonPlus.onClick.AddListener(incrementWheat);
+        WheatButtonMinus.onClick.AddListener(decrementWheat);
+        WheatText.text = WheatCounter.ToString();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void incrementWheat()
+    {
+        if(WheatCounter < 99)
+        {
+            WheatCounter++;
+            WheatText.text = WheatCounter.ToString();
+        }      
+    }
+    void decrementWheat()
+    {
+        if(WheatCounter >= 0)
+        {
+            WheatCounter--;
+            WheatText.text = WheatCounter.ToString();
+        }       
+    }
+}
