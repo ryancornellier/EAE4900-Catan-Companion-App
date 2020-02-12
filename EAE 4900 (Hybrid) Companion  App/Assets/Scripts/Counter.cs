b7a95501ffb3c0d14f38a6c0d9f379475ffe5690
Ryan Cornellier <RCTennis15@gmail.com>
2020-02-11 22:50:14 -0700

    Scene Loading & Resource Counter Progress
    Buttons naviagte to the appropriate scene when clicked. Resource Counter coding in progress, about 50-60% there? Also implemented a font that looks a lot like the one Catan uses!

---
+++ EAE 4900 (Hybrid) Companion  App/Assets/Scripts/Counter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Counter : MonoBehaviour
+{
+    public Text scoreText;
+    public int score;
+    public GameObject ButtonGameObject;
+
+    //use this for initialization
+    void Start()
+    {
+
+    }
+    //update is called once per frame
+    void Update ()
+    {
+        scoreText.text = "Wheat: " + score;
+
+        if (Input.GetMouseButtonDown(0) && EventSystem.current.currentSelectedGameObject == ButtonGameObject)
+        {
+            score++;
+        }
+
+            
+    }
+
+  
+}
