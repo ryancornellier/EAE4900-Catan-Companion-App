0240c987f6b6bae307f8a345bb248ede13f74d6d
Bree <u087067@utah.edu>
2020-02-20 02:57:04 -0700

    ALMOST

---
+++ EAE 4900 (Hybrid) Companion  App/Assets/Scripts/TradeButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TradeButton : MonoBehaviour
+{
+    public Text uiText;
+    public float mainTimer;
+
+    private float timer;
+    private bool canCount = true;
+    private bool doOnce = false;
+    public Button button;
+
+    void Start()
+    {
+        timer = Random.Range(30, 120);
+        button = GetComponent<Button>();
+        button.interactable = false;
+    }
+
+    void Update()
+    {
+        if (timer >= 0.0f && canCount)
+        {
+            timer -= Time.deltaTime;
+            uiText.text = timer.ToString("F");
+            button.interactable = false;
+        }
+
+        else if (timer <= 0.0f && !doOnce)
+        {
+            canCount = false;
+            doOnce = true;
+            button.interactable = true;
+            uiText.text = "Trade Offer";
+            timer = 0.0f;
+        }
+    }
+}
